fe303ed4e849a9cf5b570b1b3e67e2e12c1e0ad3
AntonioVUAS <108183913+AntonioVUAS@users.noreply.github.com>
2025-08-13 11:30:47 +0100

    Add files via upload

---
+++ Career page/formscript.js
@@ -0,0 +1,22 @@
+function validateForm(){
+  let fname = document.forms["application"]["firstname"].value; 
+  if(fname == ""){
+	  window.alert("Missing First Name");
+	  return false;
+  }
+  
+  let lname = document.forms["application"]["lastname"].value;
+  if(lname == ""){
+	  window.alert("Missing Last Name");
+	  return false;
+  }
+  
+  let lblemail = document.forms["application"]["email"].value;
+  if(lblemail == ""){
+	  window.alert("Missing Email");
+	  return false;
+  }
+  
+  window.alert("Thank you \n" + fname + " " + lname + "\n" + "for the apllication" + "\n" + "We will contact you!");
+  return true;
+}
